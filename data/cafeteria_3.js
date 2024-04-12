d39b901156101456dc51d18cf7f918cd7462168e
Andreyna Madera <vicandry@gmail.com>
2024-04-12 18:49:37 -0400

    maparuta

---
+++ data/cafeteria_3.js
@@ -0,0 +1,1 @@
+var json_cafeteria_3 = {"type":"FeatureCollection","name":"cafeteria_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","nombre":"panaderia dorta"},"geometry":{"type":"Point","coordinates":[-66.905311302362719,10.485786809983077]}}]}
